a5f1f72ac5aa354afee74114ec776c1febbd1dda
Daniel Falk <daniel@da-robotteknik.se>
2018-10-08 18:18:01 +0200

    Refactor tools.py
    - Use esptools as python module instead of subprocess
    - Make everything a class
    - Use argparse for arguments

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # esp8266-mqtt-home-automation-nodes
 Nodes for the ESP8266 microcontroller in micropython
+
+## Deploy
+
+First if needed, copy the micropython-libs needed, if not in your firmware, together with drivers folder and project folder:
+```
+./tools.py go weather-node drivers lib
+```
+
+Next time it is enough to deploy source for project folder
+```
+./tools.py go weather-node
+```
+
+## Flash the chip with micropython firmware
+Make sure you have a build tree with pre-built firmware image and the paths in tools.py configured. You also need 'esptools' installed with python3 support.
+
+Then run:
+```
+./tools.py reflash
+```
+
+You can also install a downloaded prebuilt binary for FW.
--- tools.py
+++ tools.py
@@ -6,133 +6,176 @@ import subprocess
 import pty
 from glob import glob
 import re
+import argparse
 
-port = "ttyUSB0"
-mpfshell = ["mpfshell", "-o", port]
-
-fw_root = "/home/daniel/workspace/esp8266/micropython/micropython/"
-target_root = os.path.join(fw_root, "ports", "esp8266")
-target_bin = os.path.join(target_root, "build", "firmware-combined.bin")
-host_root = os.path.join(fw_root, "ports", "unix")
-host_bin = os.path.join(host_root, "micropython")
+from serial.serialutil import SerialException
 
 
-def flash_chip(erase=False):
-    esptool = ["esptool.py", "--port", "/dev/%s" % port, "--baud", "115200"]
-    if erase:
-        subprocess.Popen(esptool + ["erase_flash"]).wait()
-    subprocess.Popen(esptool + ["write_flash", "--verify", "--flash_size=detect", "--flash_mode=qio", "0x00", target_bin]).wait()
+class EspTool():
+
+    def __init__(self, port, baud):
+        from esptool import main as esp
+        self.esp = esp
+        self.port = port
+        self.baud = baud
+
+    def flash(self, binary_path, mode="qio", start_addr=0x00):
+        args = ['']
+        if self.port is not None:
+            args.append("--port=/dev/%s" % self.port)
+        if self.baud is not None:
+            args.append("--baud=%d" % self.baud)
+        args.append("write_flash")
+        args.append("--flash_size=detect")
+        args.append("--flash_mode=%s" % mode)
+        args.append("0x%0.2X" % start_addr)
+        args.append(binary_path)
+        sys.argv = args
+        self.esp()
+
+    def erase(self):
+        args = ['']
+        if self.port is not None:
+            args.append("--port=/dev/%s" % self.port)
+        if self.baud is not None:
+            args.append("--baud=%d" % self.baud)
+        args.append("erase_flash")
+        sys.argv = args
+        self.esp()
 
 
-# TODO: Only first level supported
-def get_target_files():
-    p = subprocess.Popen(mpfshell + ["-n", "-c", "ls"], stdout=subprocess.PIPE)
-    out, err = p.communicate()
-    out_lines = [line for line in out.decode().split("\n") if len(line) > 0]
-    header_index = [True if "Remote files in" in l else False for l in out_lines].index(True)
-    out_lines = out_lines[header_index + 1 : ]
-    out_lines = [re.sub(r'[\x00-\x1f\x7f-\x9f][^ ]+', '', l).strip() for l in out_lines]
-    files = [(re.sub(r'<dir> ', '', l), '<dir> ' in l) for l in out_lines]
-    return files
+class Tool():
 
+    def __init__(self, port):
+        self.port = port
+        self.mpfshell = ["mpfshell", "-o", port]
 
-def has_file(name, folder_type=False):
-    files = get_target_files()
-    return (name, folder_type) in files
+        self.fw_root = "/home/daniel/workspace/esp8266/micropython/micropython/"
+        self.target_root = os.path.join(self.fw_root, "ports", "esp8266")
+        self.target_bin = os.path.join(self.target_root, "build", "firmware-combined.bin")
+        self.host_root = os.path.join(self.fw_root, "ports", "unix")
+        self.host_bin = os.path.join(self.host_root, "micropython")
 
+    def run_host(self):
+        pty.spawn([self.host_bin])
 
-def make_dir(path, error_on_exist=False):
-    name = path.split("/")
-    for i in range(0, len(name)):
-        if i == 0:
-            cmd = "md %s" % name[i]
-        else:
-            cwd = "/".join(name[:i])
-            n = name[i]
-            cmd = "cd %s; md %s" % (cwd, n)
-        p = subprocess.Popen(mpfshell + ["-n", "-c", cmd], stdout=subprocess.PIPE)
+    def run_target(self):
+        pty.spawn(self.mpfshell)
+
+    def run_repl(self):
+        pty.spawn(self.mpfshell + ["-c", "repl"])
+
+    def flash_chip(self, erase=False):
+        esp = EspTool(self.port, 115200)
+        try:
+            if erase:
+                esp.erase()
+            esp.flash(self.target_bin)
+        except SerialException:
+            print("No device connected to port")
+
+    def reset(self):
+        subprocess.Popen(self.mpfshell + ["-n", "--reset"]).wait()
+
+    def print_files(self):
+        for n, t in self.get_target_files():
+            print("%s%s" % (n, " (dir)" if t else ""))
+
+    # TODO: Only first level supported
+    def get_target_files(self):
+        p = subprocess.Popen(self.mpfshell + ["-n", "-c", "ls"], stdout=subprocess.PIPE)
         out, err = p.communicate()
-        if "Invalid directory name" in out.decode() and error_on_exist:
-            print("Failed to create directory, name already exist")
+        out_lines = [line for line in out.decode().split("\n") if len(line) > 0]
+        header_index = [True if "Remote files in" in l else False for l in out_lines].index(True)
+        out_lines = out_lines[header_index + 1:]
+        out_lines = [re.sub(r'[\x00-\x1f\x7f-\x9f][^ ]+', '', l).strip() for l in out_lines]
+        files = [(re.sub(r'<dir> ', '', l), '<dir> ' in l) for l in out_lines]
+        return files
 
+    def has_file(self, name, folder_type=False):
+        files = self.get_target_files()
+        return (name, folder_type) in files
 
-def deploy_dir(dir, type="*.py", cut_dir_name=False, recursive=True):
-    if recursive:
-        dirs = sorted([p[0] for p in os.walk(dir, followlinks=True)])
-        for d in dirs:
-            make_dir(d)
-            deploy_dir(d, type=type, recursive=False)
-    else:
-        files = glob(os.path.join(dir, type))
-        for f in files:
-            ex_dir = "."
-            if cut_dir_name:
-                f = f[len(dir) + 1 : ]
-                ex_dir = dir
-            print("cpy %s..." % f)
-            p = subprocess.Popen(mpfshell + ["-n", "-c", "put", f], cwd=ex_dir, stdout=subprocess.PIPE)
+    def make_dir(self, path, error_on_exist=False):
+        name = path.split("/")
+        for i in range(0, len(name)):
+            if i == 0:
+                cmd = "md %s" % name[i]
+            else:
+                cwd = "/".join(name[:i])
+                n = name[i]
+                cmd = "cd %s; md %s" % (cwd, n)
+            p = subprocess.Popen(self.mpfshell + ["-n", "-c", cmd], stdout=subprocess.PIPE)
             out, err = p.communicate()
-            if not "Connected" in out.decode():
-                print("Copy of files failed")
-                print(out)
-                sys.exit(3)
-            if "No such file or directory" in out.decode():
-                raise RuntimeError("File not found: %s" % f)
+            if "Invalid directory name" in out.decode() and error_on_exist:
+                print("Failed to create directory, name already exist")
 
+    def deploy_dir(self, dir, type="*.py", cut_dir_name=False, recursive=True):
+        if recursive:
+            dirs = sorted([p[0] for p in os.walk(dir, followlinks=True)])
+            for d in dirs:
+                self.make_dir(d)
+                self.deploy_dir(d, type=type, recursive=False)
+        else:
+            files = glob(os.path.join(dir, type))
+            for f in files:
+                ex_dir = "."
+                if cut_dir_name:
+                    f = f[len(dir) + 1:]
+                    ex_dir = dir
+                print("cpy %s..." % f)
+                p = subprocess.Popen(self.mpfshell + ["-n", "-c", "put", f], cwd=ex_dir, stdout=subprocess.PIPE)
+                out, err = p.communicate()
+                if"Connected" not in out.decode():
+                    print("Copy of files failed")
+                    print(out)
+                    sys.exit(3)
+                if "No such file or directory" in out.decode():
+                    raise RuntimeError("File not found: %s" % f)
 
-def deploy(project, dirs=[]):
-    if not os.path.isdir(project):
-        print("%s is not a project directory" % project)
-        sys.exit(2)
-    for d in dirs:
-        deploy_dir(d)
-    deploy_dir(project, cut_dir_name=True, recursive=False)
-    deploy_dir(project, type="*.json", cut_dir_name=True, recursive=False)
-
-
-def help():
-    print("Usage %s <command>" % sys.argv[0])
-    sys.exit(1)
+    def deploy(self, project, dirs=[]):
+        if not os.path.isdir(project):
+            print("%s is not a project directory" % project)
+            sys.exit(2)
+        for d in dirs:
+            self.deploy_dir(d)
+        self.deploy_dir(project, cut_dir_name=True, recursive=False)
+        self.deploy_dir(project, type="*.json", cut_dir_name=True, recursive=False)
 
 
 if __name__ == "__main__":
-    try:
-        cmd = sys.argv[1]
-    except IndexError:
-        help()
+    commands = ["host", "shell", "repl", "deploy", "go", "list-files", "reset", "reflash"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("command", choices=commands)
+    parser.add_argument("--port", default="ttyUSB0", help="Which port to use (ttyUSB0)")
+    args, other = parser.parse_known_args()
 
+    tool = Tool(args.port)
 
-    if cmd == "host":
-        pty.spawn([host_bin])
+    if args.command == "host":
+        tool.run_host()
 
-    elif cmd == "shell":
-        pty.spawn(mpfshell)
+    elif args.command == "shell":
+        tool.run_target()
 
-    elif cmd == "repl":
-        pty.spawn(mpfshell + ["-c", "repl"])
+    elif args.command == "repl":
+        tool.run_repl()
 
-    elif cmd == "deploy" or cmd == "go":
-        project = sys.argv[2]
-        dirs = sys.argv[3:]
-        deploy(project, dirs)
-        if cmd == "go":
-            pty.spawn(mpfshell + ["-c", "repl"])
+    elif args.command == "deploy" or args.command == "go":
+        project = other[0]
+        dirs = other[1:]
+        tool.deploy(project, dirs)
+        if args.command == "go":
+            tool.run_repl()
 
-    elif cmd == "list-files":
-        for n, t in get_target_files():
-            print("%s%s" % (n, " (dir)" if t else ""))
+    elif args.command == "list-files":
+        tool.print_files()
 
-    elif cmd == "reset":
-        subprocess.Popen(mpfshell + ["-n", "--reset"]).wait()
+    elif args.command == "reset":
+        tool.reset()
 
-    elif cmd == "reflash":
-        flash_chip(erase=True)
-
-    elif cmd == "mkdir":
-        try:
-            make_dir(sys.argv[2], error_on_exist=True)
-        except IndexError:
-            help()
+    elif args.command == "reflash":
+        tool.flash_chip(erase=True)
 
     else:
-        help()
+        raise RuntimeError("Unknown command")
