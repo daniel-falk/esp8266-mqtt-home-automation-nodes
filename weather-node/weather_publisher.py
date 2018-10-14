7c26579e87abbc291993bde330062d632b65e988
Daniel Falk <daniel@da-robotteknik.se>
2018-10-14 11:31:15 +0200

    Catch time out error on BME280 sensor
    Continue execution even if sensor times out. Do not publish any info until
    connection works again.

--- weather-node/weather_publisher.py
+++ weather-node/weather_publisher.py
@@ -1,12 +1,17 @@
 from publisher import BasePublisher
 from drivers.read_temp import sensor
+from drivers.logging import FileLogger
 
 
 class WeatherPublisher(BasePublisher):
 
     def publish(self):
+        try:
+            wdata = sensor.values
+        except OSError:
+            FileLogger.log("Failed to read weather sensor info... OSERROR")
+            return
         tags = ["temperature", "air-pressure", "humidity"]
         units = ["C", "hPa", "%"]
-        wdata = sensor.values
         data = {tag: float(value[:-len(unit)]) for tag, value, unit in zip(tags, wdata, units)}
         self.pub.publish(data, postfix=self.postfix, add_ts=True, retain=True)
